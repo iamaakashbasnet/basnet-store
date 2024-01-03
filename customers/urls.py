630e7428aa59ced4ef1583b42105698f94e5828f
Aakash Basnet <iamaakashbasnet@gmail.com>
2024-01-03 13:57:28 +0545

    feat: customer manage view

--- customers/urls.py
+++ customers/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
-from .views import CustomerListView
+from .views import (
+    CustomerListView,
+    CustomerCreateView,
+    CustomerManageView,
+)
 
 
 urlpatterns = [
     path('customers/', CustomerListView.as_view(), name='customer-list'),
+    path('customers/create/', CustomerCreateView.as_view(), name='customer-create'),
+    path('customer/manage/<int:pk>/',
+         CustomerManageView.as_view(), name='customer-manage')
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,7 +1,9 @@
-from django.views.generic import ListView
-from django.contrib.auth.mixins import (
-    LoginRequiredMixin,
-    UserPassesTestMixin,
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    View,
+    ListView,
+    CreateView,
 )
 
 from .models import Customer, Transaction
@@ -12,3 +14,18 @@ class CustomerListView(LoginRequiredMixin, ListView):
     template_name = 'customers/list_customers.html'
     context_object_name = 'customers'
     ordering = 'first_name'
+
+
+class CustomerCreateView(LoginRequiredMixin, CreateView):
+    model = Customer
+    fields = '__all__'
+    template_name = "customers/create_customer.html"
+    success_url = "/customers"
+
+
+class CustomerManageView(View):
+    def get(self, request, *args, **kwargs):
+        customer = Customer.objects.get(pk=self.kwargs.get('pk'))
+        transactions = Transaction.objects.filter(customer=customer)
+        context = {'customer': customer, 'transactions': transactions}
+        return render(request, template_name='customers/manage_customer.html', context=context)
