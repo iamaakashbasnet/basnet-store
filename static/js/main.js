df4864e18702239abc5d0eb12ffac4c6dcdc2a39
Aakash Basnet <iamaakashbasnet@gmail.com>
2024-01-07 17:11:15 +0545

    feat: loading text for button on click

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,7 @@
 document.querySelector('#currentYear').innerHTML = new Date().getFullYear();
+
+document.querySelectorAll('.btn').forEach((button) => {
+  button.addEventListener('click', (e) => {
+    e.target.textContent = 'Loading...';
+  });
+});
